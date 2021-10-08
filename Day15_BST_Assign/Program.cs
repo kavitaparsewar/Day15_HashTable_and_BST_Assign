32b9c3df81545d9346d8cfaaa4e4626240e88326
kavitaparsewar <parsewarkavita@gmail.com>
2021-10-09 00:30:30 +0530

    [kavita] added main file

---
+++ Day15_BST_Assign/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Day15_BST_Assign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Binary Search Tree\n");
+
+            //#region UC1
+
+            //UC1<int> binarySearch = new UC1<int>(56);
+
+            //binarySearch.Insert(30);
+
+            //binarySearch.Insert(70);
+
+            //binarySearch.Display();
+
+            //#endregion UC1
+
+            //#region UC2
+
+            //UC2<int> binarysearch = new UC2<int>(56);
+
+            //binarysearch.Insert(30);
+            //binarysearch.Insert(22);
+            //binarysearch.Insert(40);
+            //binarysearch.Insert(11);
+            //binarysearch.Insert(3);
+            //binarysearch.Insert(16);
+            //binarysearch.Insert(70);
+            //binarysearch.Insert(60);
+            //binarysearch.Insert(95);
+            //binarysearch.Insert(65);
+            //binarysearch.Insert(63);
+            //binarysearch.Insert(67);
+            
+            //binarysearch.Display();
+
+            //#endregion UC2
+
+
+            //#region UC3
+
+            //UC3<int> binarysearch = new UC3<int>(56);
+
+            //binarysearch.Insert(30);
+            //binarysearch.Insert(22);
+            //binarysearch.Insert(40);
+            //binarysearch.Insert(11);
+            //binarysearch.Insert(3);
+            //binarysearch.Insert(16);
+            //binarysearch.Insert(70);
+            //binarysearch.Insert(63);
+            //binarysearch.Insert(60);
+            //binarysearch.Insert(95);
+            //binarysearch.Insert(65);
+            //binarysearch.Insert(67);
+
+            //binarysearch.Display();
+
+            //bool result = binarysearch.Search(63, binarysearch);
+            //Console.WriteLine(result);
+
+            //#endregion UC3
+        }
+    }
+}
